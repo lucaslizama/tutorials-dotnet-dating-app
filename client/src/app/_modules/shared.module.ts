52fe6a2d81f5cc42630fec96a89b8369bdad5601
Lucas Lizama <lucaslizama4@gmail.com>
2023-04-01 17:01:27 -0300

    En of section 10

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -4,6 +4,7 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
 	declarations: [],
@@ -15,7 +16,10 @@ import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 		}),
 		BsDropdownModule.forRoot(),
 		NgxGalleryModule,
+		NgxSpinnerModule.forRoot({
+			type: 'line-scale-party',
+		}),
 	],
-	exports: [BsDropdownModule, ToastrModule, TabsModule, NgxGalleryModule],
+	exports: [BsDropdownModule, ToastrModule, TabsModule, NgxGalleryModule, NgxSpinnerModule],
 })
 export class SharedModule {}
